3d552e99e70a189054f2ea00b2eb9aa762b86d66
Halloway-93 <hamzaelhallaoui@gmail.com>
2026-05-28 01:19:19 +0200

    changed date format to work with android app

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -62,7 +62,7 @@ return {
 			-- Optional, if you keep daily notes in a separate directory.
 			folder = "/Daily Journal",
 			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%Y-%m-%d Week %W Day %w",
+			date_format = "%Y-%m-%d",
 			-- Optional, if you want to change the date format of the default alias of daily notes.
 			alias_format = "Week %W Day %w",
 			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
